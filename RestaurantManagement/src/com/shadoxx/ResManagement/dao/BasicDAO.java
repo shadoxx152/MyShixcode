1b2118c7efc354e0a8211346d4523170013f4ec4
shadoxx152 <shadoxx152@gmail.com>
2024-10-16 18:16:52 +0800

    update

---
+++ RestaurantManagement/src/com/shadoxx/ResManagement/dao/BasicDAO.java
@@ -0,0 +1,67 @@
+package com.shadoxx.ResManagement.dao;
+
+import com.shadoxx.demo.Dao_.Utils.JDBCUtilsByDruid;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class BasicDAO<T>{
+    private final QueryRunner queryRunner = new QueryRunner();
+
+    public int update(String sql, Object... parameters) {
+        Connection connection = null;
+
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            return queryRunner.update(connection, sql, parameters);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.close(null, null, connection);
+        }
+    }
+
+    public List<T> query(String sql, Class<T> clazz, Object... parameters) {
+        Connection connection = null;
+
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            return queryRunner.query(connection, sql, new BeanListHandler<T>(clazz), parameters);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.close(null, null, connection);
+        }
+    }
+
+    public T querySingle(String sql, Class<T> clazz, Object... parameters) {
+        Connection connection = null;
+
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            return queryRunner.query(connection, sql, new BeanHandler<T>(clazz), parameters);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.close(null, null, connection);
+        }
+    }
+
+    public Object queryScalar(String sql, Object... parameters) {
+        Connection connection = null;
+
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            return queryRunner.query(connection, sql, new ScalarHandler<>(), parameters);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.close(null, null, connection);
+        }
+    }
+}
--- RestaurantManagement/src/com/shadoxx/ResManagement/utils/Test.java
+++
@@ -1,5 +0,0 @@
-package com.shadoxx.ResManagement.utils;
-
-public class Test {
-    private final int a = 1;
-}
